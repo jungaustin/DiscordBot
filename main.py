5019c89a5a953caf19f797a4ebc05fb38e0fc34f
AusJung <137535372+jungaustin@users.noreply.github.com>
2024-10-11 04:37:25 -0700

    Created toggling for chat model, and created a daily checkin system. Will swap models to free ones later

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message, app_commands, Interaction, Embed
 from discord.ext import commands
 from responses import get_response, get_waifu
+import json
+import sqlite3
+from datetime import datetime
+
+
+
+talk_to_ai = False
 
 #Step 0: Load Out Token from Somewhere Safe
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
 
 #Step 1: Bot Setup
 intents: Intents = Intents.all()
@@ -23,7 +31,7 @@ async def send_message(message : Message, user_message : str) -> None:
         print("Message was empty because intents were not enabled")
         return
     
-    if is_private := user_message[0] == '?':
+    if is_private := user_message[0] == '!':
         user_message = user_message[1:]
         
     try:
@@ -32,14 +40,12 @@ async def send_message(message : Message, user_message : str) -> None:
     except Exception as e:
         print(e)
 
-
 @bot.hybrid_command()
 async def greet(ctx: commands.Context):
     await ctx.send("Hello, world!")
 
 @bot.hybrid_command()
 async def getwaifu(ctx: commands.Context):
-    print("in command")
     try:
         data = get_waifu()
         embed = Embed(
@@ -53,9 +59,83 @@ async def getwaifu(ctx: commands.Context):
     except Exception as e:
         print(e)
 
+def initialize(ctx: commands.Context):
+    curr_server = 'dailies_' + str(ctx.guild.id)
+    connection = sqlite3.connect('dailies.db')
+    cursor = connection.cursor()
+    cursor.execute(f'''
+                   CREATE TABLE IF NOT EXISTS {curr_server}(
+                       user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       user TEXT NOT NULL,
+                       points INTEGER NOT NULL DEFAULT 0,
+                       check_in_date TEXT)
+                   ''')
+    cursor.execute(f'''
+                   SELECT * FROM {curr_server} WHERE user = ?
+                   ''', (ctx.author.name,))
+    currUser = cursor.fetchone()
+    if not currUser:
+        cursor.execute(f'''
+                    INSERT INTO {curr_server} (user)
+                    VALUES(?)
+                    ''', (ctx.author.name,))
+        cursor.execute(f'''
+                    SELECT * FROM {curr_server} WHERE user = ?
+                    ''', (ctx.author.name,))
+        currUser = cursor.fetchone()
+    return curr_server, connection, cursor, currUser
 @bot.hybrid_command()
-async def lastLeagueMatch(ctx: commands.Context, username: str):
-    data = last_league_match(username)
+async def points(ctx: commands.Context):
+    curr_server, connection, cursor, currUser = initialize(ctx)
+    await ctx.send(f'You currently have {currUser[2]} points!')
+    connection.commit()
+    connection.close()
+    
+@bot.hybrid_command()
+async def dailies(ctx: commands.Context):
+    curr_server, connection, cursor, currUser = initialize(ctx)
+    if currUser:
+        user_id, user, points, check_in_date = currUser
+        if check_in_date is None or check_in_date != datetime.now().strftime('%Y-%m-%d'):
+            points += 100
+            check_in_date = datetime.now().strftime('%Y-%m-%d')
+            cursor.execute(f'''
+                            UPDATE {curr_server}
+                            SET
+                                points = ?,
+                                check_in_date = ?
+                            WHERE user_id = ?
+                           ''', (points, check_in_date, user_id))
+            await ctx.send(f"Hello {user}. You have successfully checked in! You currently have {points} points")
+        else:
+            await ctx.send(f"Hello {user}. You have already checked in for today.")
+            
+    
+    connection.commit()
+    connection.close()
+
+@bot.hybrid_command()
+async def gpton(ctx: commands.Context):
+    global talk_to_ai
+    talk_to_ai = True
+    await ctx.send("Sylvester Bot is now active!")
+@bot.hybrid_command()
+async def gptoff(ctx: commands.Context):
+    global talk_to_ai
+    talk_to_ai = False
+    await ctx.send("Sylvester Bot is now inactive!")
+@bot.hybrid_command()
+async def gpttoggle(ctx: commands.Context):
+    global talk_to_ai
+    talk_to_ai = not talk_to_ai
+    if talk_to_ai:
+        await ctx.send("Sylvester Bot is now active!")
+    else:
+        await ctx.send("Sylvester Bot is now inactive!")
+    
+# @bot.hybrid_command()
+# async def lastLeagueMatch(ctx: commands.Context, username: str):
+#     data = last_league_match(username)
 
 #Step 3: Handling the startup for the bot
 @bot.event 
@@ -73,21 +153,22 @@ async def on_ready() -> None:
 #Step 4: Handling incoming messages
 @bot.event
 async def on_message(message: Message) -> None:
-    if message.author == bot.user:
-        return
-    if message.content.startswith('/'):
+    if message.content.startswith(bot.command_prefix):
         await bot.process_commands(message)
         return
-    if (message.channel.id != 1264845768108544051) and (message.channel.id !=1264743004002848778):
-        return
-    
-    # return 
-    # comment out later
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
+    if talk_to_ai:
+        if message.author == bot.user:
+            return
+        if (message.channel.id != 1264845768108544051) and (message.channel.id !=1264743004002848778):
+            return
+        
+        # return 
+        # comment out later
+        username: str = str(message.author)
+        user_message: str = message.content
+        channel: str = str(message.channel)
+        print(f'[{channel}] {username}: "{user_message}"')
+        await send_message(message, user_message)
     
 #Step 5: Main Entry Point
 def main() -> None:
